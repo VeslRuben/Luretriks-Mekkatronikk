6941412ec967dcd2df2a6e81a4e59648eb3bdb95
GryfinOlai <marcusgrindvik@gmail.com>
2019-09-19 09:17:07 +0200

    Yolo created arduino code yo

--- Arduino/ArduinoGyro/ArduinoGyro.ino
+++ Arduino/ArduinoGyro/ArduinoGyro.ino
@@ -1,12 +1,30 @@
-int analogPin = 3;
-int data = 0;
+
+#include <Wire.h>
+#include <L3G.h>
+L3G gyro;
+
+
+
+
 void setup() {
   Serial.begin(9600);
-  
+  Wire.begin();
+  if (!gyro.init())
+  {
+    Serial.println("Failed to autodetect gyro type!");
+    while (1);
+  }
 
+  gyro.enableDefault();
 }
 
 void loop() {
- data = analogRead(analogPin);
- Serial.println(data);
+  gyro.read();
+  Serial.print("G ");
+  Serial.print("X");
+  Serial.print((int)gyro.g.x);
+  Serial.print("Y");
+  Serial.print((int)gyro.g.y);
+  Serial.print("Z");
+  Serial.println((int)gyro.g.z);
 }
